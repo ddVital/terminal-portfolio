89a346f33fe84bb0db80a40e605a1fd0e3aa5494
David Vital <ddvital0@gmail.com>
2024-12-23 15:00:20 -0300

    feat: initial project setup (v3) with clean folder structure

--- src/i18n/locales/index.js
+++
@@ -1,8 +0,0 @@
-
-import ptBrTranslations from './pt-br'
-import enUsTranslations from './en-us'
-
-export default {
-  'pt-BR': ptBrTranslations,  // Atente-se a usar as abreviaturas corretas
-  'en-US': enUsTranslations   // https://support.mozilla.org/pt-BR/kb/abreviacao-de-localizacao
-}
--- src/utils/cat.js
+++
@@ -1,5 +0,0 @@
-export default async function getcat() {
-    const res = await fetch('https://api.thecatapi.com/v1/images/search')
-    const resp = await res.json()
-    return resp[0].url
-}
