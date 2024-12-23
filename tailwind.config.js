2e71b11af40e9b7666e3b79326052b5f43fac25e
David Vital <ddvital0@gmail.com>
2024-12-23 16:24:14 -0300

    feat: setting theme and variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,27 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: 'var(--background-primary)',
+        secondary: 'var(--background-secondary)',
+      },
+      textColor: {
+        primary: 'var(--text-primary)',
+        secondary: 'var(--text-secondary)',
+      },
+      borderColor: {
+        primary: 'var(--border)',
+      },
+      colors: {
+        accent: {
+          primary: 'var(--accent-primary)',
+          secondary: 'var(--accent-secondary)',
+        },
+      },
+    },
   },
   plugins: [],
 }
