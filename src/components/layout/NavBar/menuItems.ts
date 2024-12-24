df41e3cf162c28d0d4be52eea486403c4cb4b177
David Vital <ddvital0@gmail.com>
2024-12-24 16:48:59 -0300

    feat(nav): set up main navbar with scroll animations and backdrop blur

---
+++ src/components/layout/NavBar/menuItems.ts
@@ -0,0 +1,7 @@
+export const menuItems = [
+  { name: 'Hero', href: '#hero' },
+  { name: 'About Me', href: '#about' },
+  { name: 'Projects', href: '#projects' },
+  { name: 'Skills', href: '#skills' },
+  { name: 'Contact Me', href: '#contact' }
+]
